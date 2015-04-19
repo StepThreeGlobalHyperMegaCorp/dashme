65d98e202ed6b8b066daad119fbae9b80d94bd80
Marc Miller <marcmill@gmail.com>
2015-04-19 15:50:30 -0400

    some cleanup

--- server.js
+++ server.js
@@ -13,23 +13,23 @@ var ectRenderer = ECT( { watch : true,
 app.set('view engine', 'ect');
 app.engine('ect', ectRenderer.render);
 
-app.use(bodyParser());
-
+// Log every request.
 app.use(function(req, res, next) {
     console.log('%s %s', req.method, req.url);
     next();
   });
 
+// These directories get used for static files.
 app.use(express.static(__dirname + "/public"))
 app.use(express.static(__dirname + "/gen"))
 app.use(express.static(__dirname + "/bower_components"))
-
 
 app.get('/',
         function(req, res) {
           res.render('home', { foo: 'foo' });
         });
 
+// Start the server.
 var g_port = Number(process.env.PORT || 3000);
 app.listen(g_port, function() {
     console.log("Listening on port %s...", g_port);
