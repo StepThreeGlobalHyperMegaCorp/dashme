4522c42aea93cac9b8df5a2df753f0eef3b7a77e
Marc Miller <marcmill@gmail.com>
2015-04-26 14:21:33 -0400

    run remote and local mongo from command line

--- run.sh
+++ run.sh
@@ -7,5 +7,19 @@ pgrep mongod || (./mongo/bin/mongod --dbpath ./mongo_files --quiet -httpinterfac
 echo \$ npm install
 npm install
 
+args=$( getopt rl $* )
+set -- $args
+
+for i; do
+    case "$i" in
+        -r)
+            ./mongo/bin/mongo candidate.1.mongolayer.com:10406/app36180534 -u lucas -pmypasswordissecure
+            exit 1;;
+        -l)
+            ./mongo/bin/mongo
+            exit 1;;
+    esac
+done
+
 echo \$ nodemon -V -w server.js $@
 node node_modules/nodemon/bin/nodemon.js -V server.js $@
