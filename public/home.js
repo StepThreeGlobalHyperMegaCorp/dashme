1404e9fddd5fd7364a7b6d325b90900b71472b70
Lucas Nelson <artimage@ishiboo.com>
2015-06-07 14:35:48 -0400

    Changed the bar chart colors.

--- public/home.js
+++ public/home.js
@@ -90,10 +90,10 @@ $( document ).ready(function () {
               datasets: [
               {
                 label: "Week 1",
-                fillColor: "rgba(220,220,220,0.5)",
-                strokeColor: "rgba(220,220,220,0.8)",
-                highlightFill: "rgba(220,220,220,0.75)",
-                highlightStroke: "rgba(220,220,220,1)",
+                fillColor: "rgba(151,187,205,0.5)",
+                strokeColor: "rgba(151,187,205,0.8)",
+                highlightFill: "rgba(151,187,205,0.75)",
+                highlightStroke: "rgba(151,187,205,1)",
                 data: $.map(last7, function(val){return val.value || 0;})
               }
               ]
